082997f9c63b67d6da723a192c396011887ea374
AndyRdn <153283134+AndyRdn@users.noreply.github.com>
2024-09-19 20:05:43 +0300

    Creation

---
+++ meuble/src/main/java/com/source/meuble/MeubleApplication.java
@@ -0,0 +1,13 @@
+package com.source.meuble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeubleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeubleApplication.class, args);
+	}
+
+}
