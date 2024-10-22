2c407022d5e09a35012cc19eaf675b5a00e04dc4
Miarantsoa Ainaharison <miarantsoaainaharison@gmail.com>
2024-10-22 09:02:35 +0300

    popover for bon-commande

--- meuble/src/main/webapp/assets/js/popover.js
+++ meuble/src/main/webapp/assets/js/popover.js
@@ -2,3 +2,9 @@ tippy("#btn", {
     content: document.querySelector("#for-validation-besoin"),
     arrow: true,
 })
+
+tippy("#btn-commande", {
+    content: document.querySelector("#for-validation-commande"),
+    arrow: true,
+    placement: 'left'
+})
