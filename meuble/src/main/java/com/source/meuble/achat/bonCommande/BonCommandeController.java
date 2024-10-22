228adeca7eb30b623a7f5b6887c26d9df495621d
NathanalexK <miarynathan@hotmail.com>
2024-10-22 00:04:45 -0700

    transferer bc

--- meuble/src/main/java/com/source/meuble/achat/bonCommande/BonCommandeController.java
+++ meuble/src/main/java/com/source/meuble/achat/bonCommande/BonCommandeController.java
@@ -1,20 +1,26 @@
 package com.source.meuble.achat.bonCommande;
 
+import com.source.meuble.achat.BonReception.BonReceptionService;
 import com.source.meuble.achat.proformat.Proformat;
 import com.source.meuble.util.Redirection;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/bon-commande")
 public class BonCommandeController {
 
     private final BonCommandeService bonCommandeService;
+    private final BonReceptionService bonReceptionService;
 
-    public BonCommandeController(BonCommandeService bonCommandeService) {
+    public BonCommandeController(BonCommandeService bonCommandeService, BonReceptionService bonReceptionService) {
         this.bonCommandeService = bonCommandeService;
+        this.bonReceptionService = bonReceptionService;
     }
 
     @GetMapping("/generer")
@@ -30,4 +36,14 @@ public class BonCommandeController {
         bonCommandeService.validerBonCommande(bonCommande);
         return new Redirection("/home/achat/validation-bon-commande").getUrl();
     }
+
+    @PostMapping("/transferer")
+    public String transferer(
+        @RequestParam("id")  BonCommande bc,
+        @RequestParam("date") LocalDate date
+    ) {
+        bonReceptionService.transferBcToBr(bc.getId(), date);
+        return new Redirection("/home/achat/validation-bon-reception").getUrl();
+    }
+
 }
